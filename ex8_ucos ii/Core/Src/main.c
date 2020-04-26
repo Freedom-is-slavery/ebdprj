19d5c323218887726ac58d011ad369e61ac894c0
Freedom-is-slavery <gzxxwwz@126.com>
2020-04-26 22:50:44 +0800

    common commit

--- ex8_ucos ii/Core/Src/main.c
+++ ex8_ucos ii/Core/Src/main.c
@@ -70,23 +70,15 @@ int main(void)
   /* USER CODE BEGIN 1 */
   OSInit();     //初始化OS
   /* USER CODE END 1 */
-
   /* MCU Configuration--------------------------------------------------------*/
-
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-
   /* USER CODE BEGIN Init */
-
   /* USER CODE END Init */
-
   /* Configure the system clock */
   SystemClock_Config();
-
   /* USER CODE BEGIN SysInit */
-
   /* USER CODE END SysInit */
-
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
