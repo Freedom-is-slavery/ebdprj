0d14287319e9a86d09ad3067471c6860755ca415
Freedom-is-slavery <gzxxwwz@126.com>
2020-05-05 16:30:21 +0800

    Add: ex9 ucos-II 温湿度计初始工作（ucos-II重新移植）

---
+++ ex9_ucos ii_Temprature/Debug/ucos-ii/port/os_dbg.d
@@ -0,0 +1,11 @@
+ucos-ii/port/os_dbg.o: ../ucos-ii/port/os_dbg.c ../ucos-ii/src/ucos_ii.h \
+ ../ucos-ii/config/app_cfg.h ../ucos-ii/config/os_cfg.h \
+ ../ucos-ii/port/os_cpu.h
+
+../ucos-ii/src/ucos_ii.h:
+
+../ucos-ii/config/app_cfg.h:
+
+../ucos-ii/config/os_cfg.h:
+
+../ucos-ii/port/os_cpu.h:
---
+++ ex9_ucos ii_Temprature/Debug/ucos-ii/src/os_mem.d
@@ -0,0 +1,11 @@
+ucos-ii/src/os_mem.o: ../ucos-ii/src/os_mem.c ../ucos-ii/src/ucos_ii.h \
+ ../ucos-ii/config/app_cfg.h ../ucos-ii/config/os_cfg.h \
+ ../ucos-ii/port/os_cpu.h
+
+../ucos-ii/src/ucos_ii.h:
+
+../ucos-ii/config/app_cfg.h:
+
+../ucos-ii/config/os_cfg.h:
+
+../ucos-ii/port/os_cpu.h:
