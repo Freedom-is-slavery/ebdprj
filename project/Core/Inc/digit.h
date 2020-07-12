57e53edd9bbf91b7eb5da625d1b6ee4a1e2fec2f
Freedom-is-slavery <gzxxwwz@126.com>
2020-07-12 13:08:28 +0800

    Add：project 树莓派气象监测系统

---
+++ project/Core/Inc/digit.h
@@ -0,0 +1,13 @@
+/**
+ * @brief digit.c所包含的头文件
+ */
+
+
+/* 分别定义四个显示的数位 */
+#define DIG1        1
+#define DIG2        2
+#define DIG3        3
+#define DIG4        4
+
+void DigitChoice(uint8_t endig);
+void DisplayOneDigit(uint8_t dig, uint8_t num);
